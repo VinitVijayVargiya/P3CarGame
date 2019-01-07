a62a8afa81b01a86ba12fca484f897d60d172177
VinitVijayVargiya <vinit.vijay.01@gmail.com>
2019-01-07 12:57:12 +0530

    Update MotiRecingGame.py
    Boundaries for both the sides are added

--- MotiRecingGame.py
+++ MotiRecingGame.py
@@ -9,6 +9,7 @@ black = (0,0,0)   #(R,G,B)
 white = (255,255,255)
 red = (255,0,0)
 
+car_width = 80
 
 # Making a window for game using pygame package
 
@@ -24,41 +25,45 @@ carImg = pygame.image.load('MyCarNew.png')
 def car(x,y):
     gameDisplay.blit(carImg,(x,y))
 
-x = (display_width  *0.45)    #these dimension are important 
-y = (display_height *0.8)    
+    
+def game_loop():
+    x = (display_width  *0.45)    #these dimension are important 
+    y = (display_height *0.8)    
 
-x_change = 0
+    x_change = 0
 
-crashed = False   
+    gameExit = False   
 
-while not crashed:
+    while not gameExit:
 
-    for event in pygame.event.get():                       # this loop will hendle the movement 
-        if event.type == pygame.QUIT:
-            crashed = True
+        for event in pygame.event.get():                       # this loop will hendle the movement (event)
+            if event.type == pygame.QUIT:
+                gameExit = True
 
-        if event.type  == pygame.KEYDOWN:  #this mean a key is pressed
-            if event.key == pygame.K_LEFT:
-                x_change = -5
+            if event.type  == pygame.KEYDOWN:  #this mean a key is pressed
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
 
-            elif event.key == pygame.K_RIGHT:
-                x_change = 5
-
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
 
 
-        #print(event) 
-    x +=  x_change 
-    gameDisplay.fill(white)   #this must be done before displaying car otherwise car will be gone
-    car(x,y)
-    pygame.display.update()    # update the display in forground  or we can use .flip() instead of .update() both will work. this line itself tells that in our game nothing is moving actually the frame is updating each time like a flip book to give feeling of motion 
-
-    clock.tick(60)     # refreshing or updating frequency (60 frames per second)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
 
 
+            #print(event) 
+        x +=  x_change 
+        gameDisplay.fill(white)   #this must be done before displaying car otherwise car will be gone
+        car(x,y)
+        if x > display_width - car_width or x < 0:
+            gameExit = True
+        pygame.display.update()    # update the display in forground  or we can use .flip() instead of .update() both will work. this line itself tells that in our game nothing is moving actually the frame is updating each time like a flip book to give feeling of motion 
+
+        clock.tick(60)     # refreshing or updating frequency (60 frames per second)
+
+game_loop()      #calling game_loop function
 pygame.quit()    # quit pygame
 
 quit()     # quit python
